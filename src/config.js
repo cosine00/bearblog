7a552d0dcba8a763ab1d3fd29e3fb4a6684fc7b0
cosine00 <124495213+cosine00@users.noreply.github.com>
2024-12-31 22:28:57 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,13 +6,14 @@ export const PandaConfig = {
     defaultLocale: 'zh',
     navbar: [
         // {title: 'Posts', url: '/'}, // auto generated
+        {title: '哔哔', url: 'https://memos.hux.ink'},
         {title: '花园', url: 'https://clip.hux.ink/'},
         {title: '悦动', url: 'https://workout.hux.ink/'},
         { title: '关于', url: '/about/' }
     ],
     footer: [
         { title: 'rss', url: '/rss.xml/' },
-        { title: 'contact', url: 'huxin1216@qq.com' },
+        { title: 'contact', url: 'https://hux.ink/' },
         { title: 'telegram', url: 'https://t.me/luckyvici' }
     ]
 }
