3866eb38860afe6e918ac9e9b7394879b7a0239c
cosine00 <huxin1216@qq.com>
2025-01-02 15:08:43 +0800

    External link

--- src/config.js
+++ src/config.js
@@ -6,14 +6,14 @@ export const PandaConfig = {
     defaultLocale: 'zh',
     navbar: [
         // {title: 'Posts', url: '/'}, // auto generated
-        {title: '哔哔', url: 'https://memos.hux.ink'},
-        {title: '花园', url: 'https://clip.hux.ink/'},
-        {title: '悦动', url: 'https://workout.hux.ink/'},
+        {title: '哔哔', url: 'https://memos.hux.ink', target: '_blank'},
+        {title: '花园', url: 'https://clip.hux.ink/', target: '_blank'},
+        {title: '悦动', url: 'https://workout.hux.ink/', target: '_blank'},
         { title: '关于', url: '/about/' }
     ],
     footer: [
         { title: 'rss', url: '/rss.xml/' },
-        { title: 'contact', url: 'https://hux.ink/' },
-        { title: 'telegram', url: 'https://t.me/luckyvici' }
+        { title: 'contact', url: 'https://hux.ink/', target: '_blank' },
+        { title: 'telegram', url: 'https://t.me/luckyvici', target: '_blank' }
     ]
 }
